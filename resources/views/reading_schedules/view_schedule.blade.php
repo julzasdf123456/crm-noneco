1690eb680ecdaefb65618b8235d7608b7daf924f
Julio Lopez <julzasdf123456@gmail.com>
2023-10-21 03:31:14 +0800

    updates'

--- resources/views/reading_schedules/view_schedule.blade.php
+++ resources/views/reading_schedules/view_schedule.blade.php
@@ -65,7 +65,7 @@
         $(document).ready(function() {
             // QUERY SCHEDS
             $.ajax({
-                url : '{{ route("/reading_schedules/get-latest-schedule") }}',
+                url : '{{ url("/reading_schedules/get-latest-schedule") }}',
                 type : 'GET',
                 data : {
                     id : "{{ $user->id }}"
