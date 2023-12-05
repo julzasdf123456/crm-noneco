17293aa24f0acf7d81e0baec7888f249a7e9dd08
Julio Lopez <julzasdf123456@gmail.com>
2023-12-06 01:58:00 +0800

    updates

--- app/Http/Controllers/ServiceConnectionMtrTrnsfrmrController.php
+++ app/Http/Controllers/ServiceConnectionMtrTrnsfrmrController.php
@@ -230,7 +230,7 @@ class ServiceConnectionMtrTrnsfrmrController extends AppBaseController
                                     'CRM_ServiceConnectionAccountTypes.AccountType as AccountType',
                                     'CRM_Barangays.Barangay as Barangay')
                     ->whereNotNull('CRM_ServiceConnections.ORNumber')
-                    ->whereNotIn('CRM_ServiceConnections.id', DB::table('CRM_ServiceConnectionMeterAndTransformer')->pluck('ServiceConnectionId'))
+                    ->whereRaw("CRM_ServiceConnections.id NOT IN (SELECT ServiceConnectionId FROM CRM_ServiceConnectionMeterAndTransformer)")
                     ->where(function ($query) {
                         $query->where('CRM_ServiceConnections.Trash', 'No')
                             ->orWhereNull('CRM_ServiceConnections.Trash');
@@ -255,7 +255,7 @@ class ServiceConnectionMtrTrnsfrmrController extends AppBaseController
                                     'CRM_ServiceConnectionAccountTypes.AccountType as AccountType',
                                     'CRM_Barangays.Barangay as Barangay')
                     ->whereNotNull('CRM_ServiceConnections.ORNumber')
-                    ->whereNotIn('CRM_ServiceConnections.id', DB::table('CRM_ServiceConnectionMeterAndTransformer')->pluck('ServiceConnectionId'))
+                    ->whereRaw("CRM_ServiceConnections.id NOT IN (SELECT ServiceConnectionId FROM CRM_ServiceConnectionMeterAndTransformer)")
                     ->where(function ($query) {
                         $query->where('CRM_ServiceConnections.Trash', 'No')
                             ->orWhereNull('CRM_ServiceConnections.Trash');
